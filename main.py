3e289365107c22f5c9e4f52ce5fe8a3fc5ee1ef9
Kiber2009 <alex2009game@mail.ru>
2024-02-12 22:05:17 +0300

    part 2

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import requests
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from ui.MainWindow import Ui_MainWindow
@@ -16,7 +17,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.spn = 0.005
         self.update_map()
 
+    def check(self):
+        if self.spn < 0:
+            self.spn = 0
+        elif self.spn > 20:
+            self.spn = 20
+
     def update_map(self):
+        self.check()
         api_address = "http://static-maps.yandex.ru/1.x/"
         params = {
             'll': f'{self.ll[0]},{self.ll[1]}',
@@ -36,6 +44,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def closeEvent(self, event):
         os.remove(MAP_NAME)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.spn -= 0.001
+            self.update_map()
+        elif event.key() == Qt.Key_PageDown:
+            self.spn += 0.001
+            self.update_map()
 
 
 if __name__ == '__main__':
